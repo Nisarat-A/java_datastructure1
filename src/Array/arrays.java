7db93c4f030f3f1a3163d2db34a7f26009cff187
Nisarat-A <noansrnn@gmail.com>
2023-05-08 09:57:56 +0700

    commit

--- src/Array/arrays.java
+++ src/Array/arrays.java
@@ -11,6 +11,11 @@ public class arrays {
         colors[3] = "yellow";
         colors[4] = "pink";
         System.out.println(Arrays.toString(colors));
+
         for (String color : colors) System.out.println(color);
+        int[] numbers = {100,200};
+
+        for(int i = colors.length-1;  i >=0; i--) System.out.println(colors[i]);
+        Arrays.stream(colors).forEach(System.out::println);
     }
 }
